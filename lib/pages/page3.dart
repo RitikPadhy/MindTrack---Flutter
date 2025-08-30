30c36f5714f8fa8ab2ca591b3937a1e3e4b84e64
RitikPadhy <ritipkp2002@gmail.com>
2025-08-30 16:23:54 +0530

    Fixes for analyze

--- lib/pages/page3.dart
+++ lib/pages/page3.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Page3 extends StatelessWidget {
-  const Page3({Key? key}) : super(key: key);
+  const Page3({super.key});
 
   @override
   Widget build(BuildContext context) {
